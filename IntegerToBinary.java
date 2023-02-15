827e2621ed68931b08c87d8b2fe3b9be3b290a99
DENNIS NGANGA <dev.dennisngugi@gmail.com>
2023-02-15 05:12:08 -0800

    Descending and Int-binary codes

---
+++ IntegerToBinary.java
@@ -0,0 +1,13 @@
+package codewars.testing;
+
+public class IntegerToBinary {
+	
+	  public static String binaryAddition(int a, int b){
+
+		    int c = a+b;
+		   
+		    //your code here
+		    return String.valueOf(Integer.toBinaryString(c));
+		  }
+
+}
